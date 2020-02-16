c75786d860d1fad7b4cb6bb8f08d665ce8e4a606
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-02-15 19:11:21 -0600

    add some actions stuff

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,10 +1,24 @@
-const { dialogflow } = require('actions-on-google');
+const { dialogflow, Suggestions, DateTime } = require('actions-on-google');
 const functions = require('firebase-functions');
 
 const app = dialogflow({ debug: true });
 
 app.intent('my_intent', (conv, { below }) => {
-    conv.close(`Getting a number below ${below}`);
+    if (below == 1) {
+        conv.close();
+    }
+    else if (below == 2) {
+        conv.followup('example-event-2', {});
+    }
+});
+
+app.intent('my_intent_2', (conv, { quit }) => {
+    if (quit == 1) {
+        conv.close();
+    }
+    else if (quit == 2) {
+        conv.followup('example-event-1', {});
+    }
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
