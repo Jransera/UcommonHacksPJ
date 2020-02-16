ee7994c1de961b6fdfda2596885f591a31adf8ba
JR <Yared.ansera@gmail.com>
2020-02-15 20:36:22 -0600

    finished encounter one with basic city merge.

--- dataMerge/merge.js
+++ dataMerge/merge.js
@@ -1,10 +1,27 @@
 //const parser = require('../parser/parse');
 const connect = require ('./connect');
+const encounters = require('../encounters');
 
 //const connections = parser.burgs();
 //const loc = {};
 (async () => {
     const trees = await connect.tree();
 
-    console.log(trees);
+    //console.log(trees);
+
+    var city = trees[Math.floor(Math.random()* trees.length)].name;
+
+    console.log("encounter:");
+     var setting= encounters[0];
+     opening = setting.encounter1.openingText.replace("{{cityName}}", city);
+
+    console.log(opening);
+
+    var choices= [setting.encounter1.choice1.choice];
+        console.log("\n\n\n");
+        console.log(choices[0]);
+    
+
+
+
 })();
